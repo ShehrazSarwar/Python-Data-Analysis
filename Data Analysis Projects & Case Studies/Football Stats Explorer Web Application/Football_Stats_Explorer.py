84d1eef5d65e98921720fdd5afb048e9de506377
Shehraz Sarwar <sheeraz23445@gmail.com>
2025-08-11 21:18:42 +0500

    Add files via upload

--- Data Analysis Projects & Case Studies/Football Stats Explorer Web Application/Football_Stats_Explorer.py
+++ Data Analysis Projects & Case Studies/Football Stats Explorer Web Application/Football_Stats_Explorer.py
@@ -41,8 +41,7 @@ st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + st
 st.dataframe(df_selected_team)
 
 if st.download_button(data = df_selected_team.to_csv(index=False),label='Download CSV File',file_name=f'NFL{selected_year}.csv'):
-    st.balloons()
-    st.success('Thanks for using our NFL Football Stats Explorer 🤗')
+    st.success('Thank You for using NFL Football Stats Explorer 🤗')
 
 cols = st.columns(3)
 with cols[0]:
